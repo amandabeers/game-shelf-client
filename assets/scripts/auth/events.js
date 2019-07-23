1aca94d7621fb2ee2db911eb80ea255478d31abb
amandabeers <amanda.green413@gmail.com>
2019-07-23 10:45:05 -0400

    Set up auth
    Set up all front end auth actions and tested that they work. The ui
    still needs work, failure and success are console logged.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,9 +1,53 @@
 'use strict'
 
+const getFormFields = require('./../../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = event => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signUp(formData)
+    .then(() => {
+      onSignIn(event)
+      ui.signUpSuccess()
+    })
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = event => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  api.signIn(formData)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = event => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  if (formData.passwords.old === formData.passwords.new) {
+    ui.samePasswordMessage()
+  } else {
+    api.changePassword(formData)
+      .then(ui.changePasswordSuccess)
+      .catch(ui.changePasswordFailure)
+  }
+}
+
+const onSignOut = () => {
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
 const addHandlers = () => {
-  $('#sign-up').on('click', onSignUp)
-  $('#sign-in').on('click', onSignIn)
-  $('#change-password').on('click', onChangePassword)
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
   $('#sign-out').on('click', onSignOut)
 }
 
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,61 @@
 'use strict'
+
+const store = require('./../store')
+
+const signUpSuccess = responseData => {
+  $('form').trigger('reset')
+  console.log('Sign up success')
+}
+
+const signUpFailure = () => {
+  $('form').trigger('reset')
+  console.log('Sign up failed')
+}
+
+const signInSuccess = responseData => {
+  $('form').trigger('reset')
+  store.user = responseData.user
+  console.log('Sign in success', store.user)
+}
+
+const signInFailure = () => {
+  $('form').trigger('reset')
+  console.log('Sign in failed')
+}
+
+const samePasswordMessage = () => {
+  $('form').trigger('reset')
+  console.log('New password must be different from old')
+}
+
+const changePasswordSuccess = () => {
+  $('form').trigger('reset')
+  console.log('Password changed')
+}
+
+const changePasswordFailure = () => {
+  $('form').trigger('reset')
+  console.log('Password change failed')
+}
+
+const signOutSuccess = () => {
+  $('form').trigger('reset')
+  console.log('Signed out')
+}
+
+const signOutFailure = () => {
+  $('form').trigger('reset')
+  console.log('Sign out failed')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  samePasswordMessage,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
